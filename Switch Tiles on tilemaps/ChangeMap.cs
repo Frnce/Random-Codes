c24f9caa1d601ef47b944d6555fd972590fbd1ae
Frnce <francetag32@gmail.com>
2019-02-05 17:03:40 +0800

    asd

---
+++ Switch Tiles on tilemaps/ChangeMap.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ChangeMap : MonoBehaviour
+{
+    public Tilemap tileMap;
+    public TileBase asd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tileMap = FindObjectOfType<Tilemap>();
+
+
+
+        BoundsInt bounds = tileMap.cellBounds;
+
+        TileBase[] allTiles = tileMap.GetTilesBlock(bounds);
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null)
+                {
+                    tileMap.SetTile(new Vector3Int(x + bounds.xMin,y + bounds.yMin,0), asd);
+                    Sprite dd = tileMap.GetSprite(new Vector3Int(x + bounds.xMin, y + bounds.yMin, 0));
+                    Debug.Log("X : " + x + " Y: " + y + " Tile : " + tile.name);
+                }
+                else
+                {
+                    Debug.Log("X : " + x + " Y: " + y + " Tile : (null)");
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Switch Tiles on tilemaps/TileSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+// Tile that repeats two sprites in checkerboard pattern
+[CreateAssetMenu]
+public class TileSwitch : TileBase
+{
+    public Tile spriteA;
+    public Tile spriteB;
+    public Sprite elsee;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        if(tilemap.GetSprite(position).name == spriteA.sprite.name)
+        {
+            tileData.sprite = spriteB.sprite;
+        }
+        else if(tilemap.GetSprite(position).name == spriteB.sprite.name)
+        {
+            tileData.sprite = spriteA.sprite;
+        }
+        else
+        {
+            tileData.sprite = elsee;
+        }
+    }
+}
